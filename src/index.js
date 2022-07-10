441419fdceec88eee37064634f148c64d61d3f9d
Bruno Lopes <eusoubruno7@gmail.com>
2022-07-10 19:30:12 -0300

    create global stykes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
+import Global from "./styles/global";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <App />
+    <Global />
+  </>,
+  document.getElementById("root")
+);
